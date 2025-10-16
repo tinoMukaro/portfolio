eb46a07b1963a314338ecb75ef7eddb5ca5a0119
tinoMukaro <vamukaro3@gmail.com>
2025-10-16 19:23:43 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar(){
+    return(
+        <nav className="nav-bar">
+            <ul className="ul-items">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+
+        </nav>
+    )
+
+}
+export default Navbar;
