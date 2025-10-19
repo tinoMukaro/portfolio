32805cd2dd189e95781ecee0f16baf018fbc8868
tinoMukaro <vamukaro3@gmail.com>
2025-10-19 11:15:15 +0200

    update project data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,18 +2,21 @@ export const projects = [
   {
     id: 1,
     title: "Mind Mirror",
-    image: "public/mindmirror.png",
+    image: "/mindmirror.png",
     shortDesc: "AI-powered journaling app for mood tracking.",
     liveLink: "https://mind-mirror-three.vercel.app/",
     repoLink: "https://github.com/tinoMukaro/mindMirror",
     details:
-      "Mind Mirror is a journaling app that uses machine learning to summarize user entries and analyze mood trends weekly. It connects a React frontend to a Express + Hugging Face backend using JWT authentication.",
+      `Mind Mirror is a journaling web app powered by machine learning. 
+      It summarizes user entries and tracks mood trends on a weekly basis. 
+      The app features a React frontend connected to an Express + Hugging Face backend, secured with JWT authentication, 
+      Arcjet for rate limiting and bot protection, and Zod for input validation.`,
     techStack: ["React", "Express", "Hugging Face API"],
   },
   {
     id: 2,
     title: "Traffic Offence Management System (TOMS)",
-    image: "public/toms.png",
+    image: "/toms.jpg",
     shortDesc: "A web system to manage and track traffic offences.",
     repoLink: "https://github.com/tinoMukaro/TOMS",
     details:
